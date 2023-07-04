3279c8eac1f63a08cd66eea559ede07412d1ede8
Yash <sharmaji9833@gmail.com>
2023-07-04 18:28:48 +0530

    video-03-Create_React_App

---
+++ my-react-app/src/App.js
@@ -0,0 +1,14 @@
+
+
+function App() {
+  return (
+    <div >
+      <header >
+        <a href="/.">amazon</a>
+      </header>
+      <main>list product</main>
+    </div>
+  );
+}
+
+export default App;
